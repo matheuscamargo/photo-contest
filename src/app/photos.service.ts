8931395689947cdb638fc39029600fced76be375
Matheus Camargo <mamende@microsoft.com>
2019-06-13 22:34:21 -0700

    Implement basic prototype of main idea

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -5,12 +5,16 @@ import { Photo } from './photo';
   providedIn: 'root'
 })
 
-export class PhotosService {
+export class PhotosService {  
   PHOTOS: Photo[] = [
     { index: 0, filePath: "0" },
     { index: 1, filePath: "1" },
     { index: 2, filePath: "2" },
-    { index: 3, filePath: "3" }
+    { index: 3, filePath: "3" },
+    { index: 4, filePath: "4" },
+    { index: 5, filePath: "5" },
+    { index: 6, filePath: "6" },
+    { index: 7, filePath: "7" }
   ];
 
   constructor() { }
--- src/app/voting/voting.component.ts
+++ src/app/voting/voting.component.ts
@@ -9,21 +9,80 @@ import { PhotosService } from '../photos.service';
 })
 export class VotingComponent implements OnInit {
 
-  photos: Photo[];
+  // To display.
   photo1: Photo;
   photo2: Photo;
-  votedFor: number;
+  votedFor = -1;
+  
+  allPhotos: Photo[];
+  currentPhotos: Photo[];
+  result: Photo[];
+  nextLevel: Photo[];
 
   constructor(private photoService: PhotosService) { }
 
   ngOnInit() {
-    this.photos = this.photoService.getPhotos();
-    this.photo1 = this.photos[0];
-    this.photo2 = this.photos[1];
-    this.votedFor = -1;
+    this.allPhotos = this.photoService.getPhotos();
+    this.initializeVoting();
   }
 
   onVoted(photo: number) {
-    this.votedFor = photo;
+    this.applyVote(photo);
+  }
+
+  initializeVoting() {
+    this.currentPhotos = this.allPhotos;
+    this.result = new Array();
+    this.nextLevel = new Array();
+    this.updateView();
+  }
+
+  applyVote(votedForIndex: number) {
+    // Logic only supports powers of 2.
+    var votedFor: Photo;
+    var notVotedFor: Photo;
+
+    if (this.currentPhotos.length == 0) {
+      return;
+    }
+
+    if (votedForIndex == 1) {
+      notVotedFor = this.currentPhotos.pop();
+      votedFor = this.currentPhotos.pop();
+    }
+    else if (votedForIndex == 2) {
+      votedFor = this.currentPhotos.pop();
+      notVotedFor = this.currentPhotos.pop();
+    }
+    else {
+      // Add error.
+      return;
+    }
+
+    this.result.push(notVotedFor);
+    this.nextLevel.push(votedFor);
+
+    if (this.currentPhotos.length == 0) {
+      this.currentPhotos = this.nextLevel;
+      this.nextLevel = new Array();
+    }
+    
+    if (this.currentPhotos.length == 1) {
+      this.result.push(this.currentPhotos.pop());
+      return;
+    }
+
+    this.updateView();
+  }
+
+  updateView() {
+    var currentLevelSize = this.currentPhotos.length;
+    if (currentLevelSize < 2) {
+      // Finish logic.
+      return;
+    }
+
+    this.photo1 = this.currentPhotos[currentLevelSize - 2];
+    this.photo2 = this.currentPhotos[currentLevelSize - 1];
   }
 }
