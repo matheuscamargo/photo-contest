f08c7161b7deb0fb41b699b8ca9bf6da8700c01b
Matheus Camargo <mamende@microsoft.com>
2019-06-13 19:52:08 -0700

    Set voting component to root route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VotingComponent } from './voting/voting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: VotingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
